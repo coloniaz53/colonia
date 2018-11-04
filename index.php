5e48d7e89bb7a583c73cb1000da76cfc78656ab0
Adeilton de Araujo <coloniaz53@coloniaz53.com>
2018-11-04 18:43:20 -0300

    mais teste

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
 
- echo "Hello World!";
-<<<<<<< HEAD
-=======
- echo "Adeilton de Araujo";
- echo "tem que dá certo"
->>>>>>> 94cc65308de713265c229e6983e6d31f4a892353
+ echo "Hello World e Adeilton de Araujo!";
+
+  echo "local deu certo"
+
 
 >
